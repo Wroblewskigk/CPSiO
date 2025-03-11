3ffbd389d8a0fdd150d6470ce6e62433745389b3
wroblewskigk <wroblewskigk@proton.me>
2025-03-11 10:20:20 +0100

    Made Z1 schematic for L01

--- L01/__init__.py
+++ L01/__init__.py
@@ -1,19 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-PLIK = "./egk1.txt"
+PLIK = "./ekg1.txt"
 FS = 1000
 XSCALE = 1
 YSCALE = 1
-BEGIN = 10
-END = 20
+BEGIN = 1000
+END = 2000
 RANGE = 12
 
-for i in range(1, RANGE):
+# Z1
+ekg1 = []  # Store all rows
+with open(PLIK, "r") as file:
+    for line in file:
+        numbers = list(map(int, line.split()))  # Convert split strings into integers
+        ekg1.append(numbers)  # Store the row
 
-
-xpoints = np.array([1, 8])
-ypoints = np.array([3, 10])
-
-plt.plot(xpoints, ypoints)
-plt.show()
+ekg1 = ekg1[BEGIN:END]
+for columnIndex in range(0, 11):
+    ypoints = [row[columnIndex] for row in ekg1 if len(row) > columnIndex]
+    xpoints = list(range(len(ypoints)))
+    plt.plot(xpoints, ypoints)
+    plt.xlabel("Fs [kHz]")
+    plt.ylabel("To w czym mierzymy EKG")
+    plt.title("Wykres sygnału EKG mierzony z częstotliwością 1kHz")
+    plt.show()
